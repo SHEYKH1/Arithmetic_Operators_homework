863707de245a54497cd2d9fd0a8d8c54bd409b8c
SHEYKH1 <ozodbekotaboyev007@gmail.com>
2023-11-09 15:45:15 +0500

    added solution

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,7 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+a=int(input())
+b=a%10
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+x=a//10
+print(b,x)
 #Print the answer variable
